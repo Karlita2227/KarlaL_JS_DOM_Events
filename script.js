f709e482171eae99b5b0531140d4dfdd0246f2cd
[Karla Lewis] <[karlaoglivie@gmail.com]>
2025-04-03 17:19:41 -0700

    Updated JavaScript for Random Color Change

--- script.js
+++ script.js
@@ -20,12 +20,9 @@ document.addEventListener("click", function(e) {
 
 });
 
-randomColorBtn.addEventListener("click", () => {
-    document.body,style.backgroundColor = generateRandomColor ();
-})
-
-function generateRandomColor() {
-    let randomColorBtn = "#" + Math.floor(Math.random() * 16777215).toString(16);
-    return randomColor;
-}
-
+function changeBackgroundColor() {
+    let randomColor = "#" + Math.floor(Math.random()*16777215).toString(16); 
+    document.body.style.backgroundColor = randomColor; 
+  }
+  
+  document.addEventListener('click', changeBackgroundColor); 
