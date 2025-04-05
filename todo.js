b32fa88455c3289d269525c803a87f360eb8ab7b
[Karla Lewis] <[karlaoglivie@gmail.com]>
2025-04-05 11:22:27 -0700

    Updating todo.js

---
+++ todo.js
@@ -0,0 +1,15 @@
+function createListItem(taskText) {
+    return result;
+  }
+
+let listItem = document.createElement('li');
+
+listItem.textContent = 'This is a list item';
+listItem.textContent = 'Home';
+listItem.textContent = 'Second Page';
+listItem.textContent = 'Disneyland';
+listItem.textContent = 'Fun Things to Do';
+
+listItem.setAttribute('class', 'my-list-item'); 
+
+console.log(listItem);
